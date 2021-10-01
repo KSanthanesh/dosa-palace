460d6b4cb9465274fb081046186ad5349ca4a3a6
KSanthanesh <santhanesh_2004@yahoo.com>
2021-10-01 15:40:39 +0000

    Add models,views,admin and urls.py

--- meals/models.py
+++ meals/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Meals(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    description = models.TextField(max_length=200)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    preparation_time = models.IntegerField()
+    image = models.ImageField(upload_to='media/', default=True)
+
+    class Meta:
+        verbose_name = 'meal'
+        verbose_name_plural = 'meals'
+
+    def __str__(self):
+        return self.name
