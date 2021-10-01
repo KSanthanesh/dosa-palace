f1fa27a0fad957f0ea3ed1c90aea8259e7f65a5a
KSanthanesh <santhanesh_2004@yahoo.com>
2021-10-01 21:41:38 +0000

    Add and style Meals page

--- meals/urls.py
+++ meals/urls.py
@@ -1,8 +1,10 @@
 from . import views
 from django.urls import path
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('get_meal_list/', views.get_meal_list, name='get_meal_list'),
-    path('get_meal_detail/<int:m_id>', views.get_meal_detail, name='get_meal_detail'),
+    path('get_meal_detail/<int:m_id>', views.get_meal_detail, name='get_meal_detail'),  # noqa: E501
 ]
