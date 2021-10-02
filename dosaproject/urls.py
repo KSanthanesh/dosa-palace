84888fc7364e3a6c24530d5684d08ca8d45535c1
KSanthanesh <santhanesh_2004@yahoo.com>
2021-10-02 18:19:57 +0000

    Add account registration

--- dosaproject/urls.py
+++ dosaproject/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('', include('meals.urls'), name='meal-urls'),
     path('admin/', admin.site.urls),
     path('meals/', include('meals.urls'), name='meal-urls'),
+    path('accounts/', include('allauth.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
