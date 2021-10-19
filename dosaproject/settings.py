66eb15cf8d25bcffa8ef2e12dc08df1cee89d400
KSanthanesh <santhanesh_2004@yahoo.com>
2021-10-19 12:44:01 +0000

    Deployment commit

--- dosaproject/settings.py
+++ dosaproject/settings.py
@@ -100,8 +100,9 @@ WSGI_APPLICATION = 'dosaproject.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-		'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-		}
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
+
 # if development:
 #     DATABASES = {
 #         'default': {
