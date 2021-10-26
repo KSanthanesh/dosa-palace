7f88fe07e8e3a2769a99900c8dd94c69448d0c17
KSanthanesh <santhanesh_2004@yahoo.com>
2021-10-26 13:12:11 +0000

    Format all html files and css file

--- meals/apps.py
+++ meals/apps.py
@@ -1,6 +1,13 @@
+"""
+apps.py
+"""
 from django.apps import AppConfig
 
 
 class MealsConfig(AppConfig):
+    """
+    Class MealsConfig
+    """
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'meals'
